d7bf8a67ef9075e3bd36714668296ea7dba06282
chaerin <70943835+chaerin-dev@users.noreply.github.com>
2022-04-01 12:11:33 +0900

    Create 문자열을 정수로 바꾸기.js

---
+++ LEVEL1/문자열을 정수로 바꾸기.js
@@ -0,0 +1,14 @@
+function solution(s) {
+  return Number(s);
+}
+
+/* 좋았던 다른 사람의 풀이:
+꼭 더 좋은 풀이라기보다는 Number와의 사칙연산을 통해
+자동으로 형 변환이 되는 점을 이용한 것이 인상깊었다.
+
+function strToInt(str){
+  return str/1;
+  // 또는 return +str;
+  // 또는 return str-0; 등...
+}
+*/
