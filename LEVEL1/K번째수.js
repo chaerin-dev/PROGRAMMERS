85559afe3e9f72bb2308de145b14529d4d0a9ec2
chaerin <70943835+chaerin-dev@users.noreply.github.com>
2022-04-28 17:59:48 +0900

    Update K번째수.js

--- LEVEL1/K번째수.js
+++ LEVEL1/K번째수.js
@@ -11,3 +11,17 @@ function solution(array, commands) {
   }
   return answer;
 }
+
+/* 좋았던 다른 사람의 풀이:
+풀이 자체는 내 풀이와 완벽히 일치하지만,
+map 메서드를 이용해서 while문 없이 여러 테스트케이스를 처리한 점이 인상깊었다.
+
+function solution(array, commands) {
+  return commands.map((v) => {
+    return array
+      .slice(v[0] - 1, v[1])
+      .sort((a, b) => a - b)
+      .slice(v[2] - 1, v[2])[0];
+  });
+}
+*/
