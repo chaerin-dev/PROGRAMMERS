0d51c712a17039524a184f27fe86c8008d6d2268
chaerin <70943835+chaerin-dev@users.noreply.github.com>
2022-05-25 16:30:02 +0900

    Update 소수 만들기.js

--- LEVEL1/소수 만들기.js
+++ LEVEL1/소수 만들기.js
@@ -13,8 +13,8 @@ const getCombinations = function (arr, selectNumber) {
 
 // num이 소수인지 여부를 반환하는 함수
 function isPrime(num) {
-  if (num === 2) return true;
-  for (let i = 2; i <= Math.floor(Math.sqrt(num)); i++) {
+  if (num === 1) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
     if (num % i === 0) return false;
   }
   return true;
