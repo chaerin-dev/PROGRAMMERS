4ab8f63d53a52bc834d4a923e190a94d7e662cef
chaerin-dev <chaerin.dev@gmail.com>
2023-03-23 20:48:46 +0900

    Update 리코쳇 로봇.js

--- LEVEL2/리코쳇 로봇.js
+++ LEVEL2/리코쳇 로봇.js
@@ -6,6 +6,7 @@ const MOVE = [
   [0, 1],
 ];
 
+// direction 방향으로 board를 벗어나거나 장애물(D)을 만나기 직전까지 이동 후 도착지점 반환
 const move = (board, currentPosition, direction) => {
   const [ROWS, COLS] = [board.length, board[0].length];
 
@@ -22,12 +23,15 @@ const move = (board, currentPosition, direction) => {
 function solution(board) {
   const [ROWS, COLS] = [board.length, board[0].length];
 
+  // board 각 지점의 방문 여부를 저장할 2차원 배열
   const visited = Array.from({ length: ROWS }, () =>
     Array.from({ length: COLS }, () => false)
   );
 
+  // BFS를 위한 queue
   const queue = [];
 
+  // 시작점(R) 찾기
   for (let i = 0; i < ROWS; i++) {
     for (let j = 0; j < COLS; j++) {
       if (board[i][j] === "R") {
@@ -37,7 +41,7 @@ function solution(board) {
     }
   }
 
-  // bfs
+  // BFS
   while (true) {
     const {
       pos: [CR, CC],
